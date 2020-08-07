90e4b13b7e0ada4e61cee8676a0a22e15fa32154
Lucius Choi <lucius.choi@gmail.com>
2020-08-07 18:11:47 +0900

    prepared for deployment

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ ruby '2.7.1'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 # Use postgresql as the database for Active Record
-gem 'pg', '>= 0.18', '< 2.0'
+gem 'mysql2', '>= 0.4.4'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
@@ -52,3 +52,9 @@ end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+
+
+gem 'capistrano', '~> 3.11'
+gem 'capistrano-rails', '~> 1.4'
+gem 'capistrano-passenger', '~> 0.2.0'
+gem 'capistrano-rbenv', '~> 2.1', '>= 2.1.4'
---
+++ config/deploy.rb
@@ -0,0 +1,61 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.1"
+
+set :application, "weblog"
+set :repo_url, "git@github.com:luciuschoi/weblog.git"
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+set :deploy_to, "/home/deployer/#{fetch :application}"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
+
+namespace :deploy do
+  namespace :check do
+    before :linked_files, :set_production_key do
+      on roles(:app), in: :sequence, wait: 10 do
+        unless test("[ -f #{shared_path}/config/credentials/production.key ]")
+          upload! 'config/credentials/production.key', "#{shared_path}/config/credentials/production.key"
+        end
+      end
+    end
+  end
+
+  # desc 'Config bundler'
+  # task :config_bundler do
+  #   on roles(/.*/) do
+  #     execute :bundle, 'config', '--local deployment', true
+  #     execute :bundle, 'config', '--local', 'without', "development:test"
+  #     execute :bundle, 'config', '--local', 'path', shared_path.join('bundle')
+  #   end
+  # end
+end
